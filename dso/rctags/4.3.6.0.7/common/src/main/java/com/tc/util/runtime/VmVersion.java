707158cd39cb5f82d54dd6642a21c095818aa483
cruise <cruise@7fc7bbf3-cf45-46d4-be06-341739edd864>
2018-03-23 11:18:15 +0000

    creating tag for 4.3.6.0.7

---
+++ dso/rctags/4.3.6.0.7/common/src/main/java/com/tc/util/runtime/VmVersion.java
@@ -0,0 +1,73 @@
+/* 
+ * The contents of this file are subject to the Terracotta Public License Version
+ * 2.0 (the "License"); You may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at 
+ *
+ *      http://terracotta.org/legal/terracotta-public-license.
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for
+ * the specific language governing rights and limitations under the License.
+ *
+ * The Covered Software is Terracotta Platform.
+ *
+ * The Initial Developer of the Covered Software is 
+ *      Terracotta, Inc., a Software AG company
+ */
+package com.tc.util.runtime;
+
+import java.util.Locale;
+import java.util.Properties;
+
+/**
+ * Stores parsed version information
+ */
+public final class VmVersion {
+
+  private final boolean        isIBM;
+  private final boolean        isJRockit;
+
+  /**
+   * Construct with system properties, which will be parsed to determine version. Looks at properties like java.version,
+   * java.runtime.version, jrockit.version, java.vm.name, and java.vendor.
+   *
+   * @param props Typically System.getProperties()
+   */
+  public VmVersion(final Properties props) {
+    this(isJRockit(props), isIBM(props));
+  }
+
+  /**
+   * Construct with specific version information
+   *
+   * @param isJRockit True if BEA JRockit JVM
+   * @param isIBM True if IBM JVM
+   */
+  private VmVersion(final boolean isJRockit, final boolean isIBM) {
+    this.isIBM = isIBM;
+    this.isJRockit = isJRockit;
+  }
+
+  /**
+   * @return True if IBM JVM
+   */
+  public boolean isIBM() {
+    return isIBM;
+  }
+
+  /**
+   * @return True if BEA JRockit
+   */
+  public boolean isJRockit() {
+    return isJRockit;
+  }
+
+  private static boolean isIBM(Properties props) {
+    return props.getProperty("java.vm.name", "").toLowerCase(Locale.ENGLISH).contains("ibm");
+  }
+
+  private static boolean isJRockit(Properties props) {
+    return props.getProperty("jrockit.version") != null
+           || props.getProperty("java.vm.name", "").toLowerCase(Locale.ENGLISH).indexOf("jrockit") >= 0;
+  }
+}
