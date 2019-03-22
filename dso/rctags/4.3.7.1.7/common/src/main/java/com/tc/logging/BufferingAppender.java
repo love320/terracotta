94c1a7acaf5ae175082b9b4090298451f1bda836
cruise <cruise@7fc7bbf3-cf45-46d4-be06-341739edd864>
2019-03-22 11:17:08 +0000

    creating tag for 4.3.7.1.7

---
+++ dso/rctags/4.3.7.1.7/common/src/main/java/com/tc/logging/BufferingAppender.java
@@ -0,0 +1,63 @@
+/*
+ * The contents of this file are subject to the Terracotta Public License Version
+ * 2.0 (the "License"); You may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at
+ *
+ *      http://terracotta.org/legal/terracotta-public-license.
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for
+ * the specific language governing rights and limitations under the License.
+ *
+ * The Covered Software is Terracotta Platform.
+ *
+ * The Initial Developer of the Covered Software is
+ *      Terracotta, Inc., a Software AG company
+ */
+package com.tc.logging;
+
+import org.apache.logging.log4j.core.Appender;
+import org.apache.logging.log4j.core.Filter;
+import org.apache.logging.log4j.core.Layout;
+import org.apache.logging.log4j.core.LogEvent;
+import org.apache.logging.log4j.core.appender.AbstractAppender;
+
+import java.io.Serializable;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+
+/**
+ * An {@link Appender} that simply buffers records (in a bounded queue) until they're needed. This is used for making
+ * sure all logging information gets to the file; we buffer records created before logging gets sent to a file, then
+ * send them there.
+ */
+
+public class BufferingAppender extends AbstractAppender {
+
+  private final BlockingQueue<LogEvent> buffer;
+  private boolean on;
+
+  public BufferingAppender(int capacity, String name, Filter filter, Layout<? extends Serializable> layout, boolean ignoreExceptions) {
+    super(name, filter, layout, ignoreExceptions);
+    this.buffer = new ArrayBlockingQueue<LogEvent>(capacity);
+    this.on = true;
+  }
+
+  public synchronized void append(LogEvent logEvent) {
+    if (on) {
+      this.buffer.offer(logEvent.toImmutable());
+    }
+  }
+
+  public void stopAndSendContentsTo(Appender otherAppender) {
+    synchronized (this) {
+      on = false;
+    }
+
+    while (true) {
+      LogEvent event = this.buffer.poll();
+      if (event == null) break;
+      otherAppender.append(event);
+    }
+  }
+}
